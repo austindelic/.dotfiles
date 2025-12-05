e564f23cc298970aa715deb2df4d0ff1c5c0f7c1
Austin Delic <austin@austindelic.com>
2025-12-05 16:26:08 +0800

    from air

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.g.neovide_opacity = 1
-vim.g.transparency = 1
-
-vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h13"
-vim.g.neovide_cursor_animation_length = 0.04
-vim.g.neovide_cursor_trail_size = 0.25
-vim.g.neovide_window_blurred = true
-vim.env.PATH = vim.fn.expand("~/.asdf/shims") .. ":" .. vim.env.PATH
-vim.api.nvim_set_hl(0, "TreesitterContext", { link = "Normal" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.neovide_opacity = 1
+vim.g.transparency = 1
+vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h13"
+vim.g.neovide_cursor_animation_length = 0.04
+vim.g.neovide_cursor_trail_size = 0.25
+vim.g.neovide_window_blurred = true
+vim.env.PATH = vim.fn.expand("~/.asdf/shims") .. ":" .. vim.env.PATH
+vim.api.nvim_set_hl(0, "TreesitterContext", { link = "Normal" })
